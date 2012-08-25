861b646b52b16dc1e7afbe496e0fd024a1534c37
Harut Martirosyan <harut.martirosyan@gmail.com>
2012-08-25 07:03:54 +0400

    initial commit

---
+++ src/main/java/com/asrmscii2unicode/App.java
@@ -0,0 +1,13 @@
+package com.asrmscii2unicode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
