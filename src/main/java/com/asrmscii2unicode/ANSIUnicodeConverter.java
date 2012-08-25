c9ea0d90330dccd5f2a2558d187e331c7dc5d4f4
Harut Martirosyan <harut.martirosyan@gmail.com>
2012-08-25 08:47:44 +0400

    Added basic functionality

---
+++ src/main/java/com/asrmscii2unicode/ANSIUnicodeConverter.java
@@ -0,0 +1,11 @@
+package com.asrmscii2unicode;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: inteloid
+ * Date: 8/25/12
+ * Time: 7:21 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ANSIUnicodeConverter {
+}
